d710b696b304235204ef4583ef1074468cc4bc23
Peeter Tinits <peeter.tinits@gmail.com>
2023-08-02 17:11:34 +0300

    added people overview

--- code/meta_generate_reports.R
+++ code/meta_generate_reports.R
@@ -4,3 +4,5 @@ rmarkdown::render("code/overview_ENB_1525-2021.Rmd", output_format = "html_docum
 file.rename(from="code/overview_ENB_1525-2021.html",to="reports/overview_ENB_1525-2021.html")
 rmarkdown::render("code/overview_ENB_1525-2021.Rmd", output_format="pdf_document")
 file.rename(from="code/overview_ENB_1525-2021.pdf",to="reports/overview_ENB_1525-2021.pdf")
+rmarkdown::render("code/overview_ENB_people.Rmd", output_format = "html_document")
+file.rename(from="code/overview_ENB_people.html",to="reports/overview_ENB_people.html")
